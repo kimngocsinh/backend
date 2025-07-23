25169e4eba5d86e9ddb8c5b2a934f731c5bc6f61
kimngocsinh <kimngocsinh@luvina.net>
2025-07-23 16:12:35 +0700

    update exception

--- src/main/java/com/springboot/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/backend/exception/GlobalExceptionHandler.java
@@ -2,12 +2,15 @@ package com.springboot.backend.exception;
 
 import com.springboot.backend.dto.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -22,14 +25,44 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidationExceptions(MethodArgumentNotValidException e,
                                                                         HttpServletRequest request) {
-            String errorMessage = e.getBindingResult()
-                                    .getFieldErrors()
-                                    .stream()
-                                    .map(err -> err.getField() + " " + err.getDefaultMessage())
-                    .findFirst()
-                    .orElse("Error Validation");
-            return ResponseEntity.badRequest().body(
-                    ApiResponse.error(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), errorMessage)
-            );
+
+        var optionalFieldError = e.getBindingResult().getFieldErrors().stream().findFirst();
+
+        String errorMessage = optionalFieldError
+                .map(err -> err.getField() + " " + err.getDefaultMessage())
+                .orElse("Validation error");
+
+        if (optionalFieldError.isPresent()) {
+            try {
+                // Thử unwrap lỗi về ConstraintViolation để lấy attributes
+                ConstraintViolation<?> violation = optionalFieldError.get().unwrap(ConstraintViolation.class);
+                Map<String, Object> attributes = violation.getConstraintDescriptor().getAttributes(); // trả về map [message = "...", min=3, ...]
+
+                errorMessage = mapAttribute(errorMessage, attributes);
+            } catch (Exception ex) {
+                // unwrap không thành công thì dùng nguyên message ban đầu
+            }
+        }
+
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), errorMessage, request.getRequestURI()));
+    }
+
+    /**
+     * Custom message: vd tuoi phai tren 18,... dưựa vào giá trị trong annotation
+     * @param message
+     * @param attributes
+     * @return message
+     */
+    private String mapAttribute (String message, Map<String, Object> attributes) {
+        String minValue = attributes.get("min").toString();
+        String maxValue = attributes.get("max").toString();
+        if (minValue != null) {
+            message = message.replace("{min}", minValue);
+        }
+        if (maxValue != null) {
+            message = message.replace("{max}", maxValue);
+        }
+        return message;
     }
 }
